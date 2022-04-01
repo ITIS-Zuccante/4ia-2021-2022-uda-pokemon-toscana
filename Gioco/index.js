cedb5f659cd795488d21a873fc88e9d292af6587
Banjo <sebastiano.bortoletti@itiszuccante.edu.it>
2022-04-01 20:38:23 +0200

    index

--- Gioco/index.js
+++ Gioco/index.js
@@ -1,39 +1,7 @@
-const openModalButton = document.getElementById("helpButton")
-const closeModalButton = document.getElementById("closeButton")
-const nextPageButton = document.getElementById("nextPage")
-const previousPageButton = document.getElementById("previousPage")
-const text = document.getElementById("modalContainer")
-const modal = document.getElementById("modal");
-const button = document.querySelector('button')
+const team1Button = document.getElementById('team1');
+const team2Button = document.getElementById('team2');
 
-
-let currentPage = 1;
-let maxPages = 3;
-
-openModalButton.addEventListener('click', () =>{
-    modal.style.display = "block";
-    button.disabled = true;
+team1Button.addEventListener('click', () => {
+    team1Button.style.color = "#4998E0";
+    alert();
 })
-
-closeModalButton.addEventListener('click', () =>{
-    modal.style.display = "none"
-    button.disabled = false;
-})
-
-nextPageButton.addEventListener('click', () =>{
-    if(currentPage < maxPages) {
-        currentPage++;
-        displayPage();
-    }
-})
-
-previousPageButton.addEventListener('click', () =>{
-    if(currentPage > 1){
-        currentPage--
-        displayPage()
-    }
-})
-
-function displayPage(){
-    text.style.backgroundImage = `url('/images/writings/rules/rules_${currentPage}.png')`;
-}
