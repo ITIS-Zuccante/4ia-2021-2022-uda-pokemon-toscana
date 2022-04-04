82c64669302f1fc837f762e5934a6e1305b7a3fa
Banjo <sebastiano.bortoletti@itiszuccante.edu.it>
2022-04-04 16:00:20 +0200

    h

--- Gioco/index.js
+++ Gioco/index.js
@@ -1,7 +1,20 @@
 const team1Button = document.getElementById('team1');
 const team2Button = document.getElementById('team2');
+const rulesButton = document.getElementById('rules');
+const fightButton = document.getElementById('fight');
 
-team1Button.addEventListener('click', () => {
-    team1Button.style.color = "#4998E0";
-    alert();
-})
+const buttons = [team1Button, team2Button, rulesButton, fightButton];
+
+buttons.map( button => {
+    button.addEventListener('click', (text) => {
+        button.style.backgroundColor = "#F93C3C";
+        for (let index = 0; index < buttons.length; index++) {
+            console.log(buttons[index] == button);
+            if(buttons[index] == button){
+                buttons[index].style.backgroundColor = "#F93C3C";
+            } else{
+                buttons[index].style.backgroundColor = "#4998E0";
+            }            
+        }
+    });
+});
